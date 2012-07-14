f898a526516e6d2375137a2ef2df40baffca795f
Ryan Scheel <ryan.havvy@gmail.com>
2012-07-14 05:30:06 -0700

    Gave a directory structure to keep the source, test file, and library seperate.
    Added #[link] metadata to remove warnings, and be standard with other libs.

--- src/bimap.rs
+++ src/bimap.rs
@@ -1,10 +1,9 @@
-/**
-Author: Havvy
+#[link(name = "bimap", vers = "0.9", author = "Havvy")];
 
+/**
 This is an implementation of a bimap done using two hashmaps. I cannot speak
 to its speed or size or any characteristics you care about if you care about
-code quality. Use this when you need a bimap, and performance doesn't matter
-yet. Unless another bimap implementation comes outs by somebody more studied.
+code metrics. Use this when you need a bimap, and performance doesn't matter.
 */
 
 use std;
